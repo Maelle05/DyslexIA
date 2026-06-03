9ee28bf14c24bb6a3c50d9b46f78380768c45825
Yoann Lemoine <lemoine.yoann@yahoo.fr>
2026-06-03 17:21:56 +0200

    finished implementing model class, trained a local model and used it to make predictions

--- dyslexia/interface/main.py
+++ dyslexia/interface/main.py
@@ -1,3 +1,32 @@
+from dyslexia.processing.process_data_justine import get_files_path, process_dataset, split
+from dyslexia.model.xgboost import XGBoostModel
+from sklearn.preprocessing import StandardScaler
+
 def main():
-    print("Hello Dyslexia Detection!")
-    # TODO ADD APP LOGIC HERE
+    files_metrics, files_fixations = get_files_path("/home/yoannl/code/drealKn/DyslexIA/data/data")
+    dataset = process_dataset(files_metrics, files_fixations)
+    X_train, X_test, y_train, y_test = split(dataset)
+
+    scaler  = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test  = scaler.transform(X_test)
+
+
+    """model = XGBoostModel(X_train, y_train)
+    train_acc, test_acc = model.evaluate(X_train, y_train, X_test, y_test)
+    print(f"Train Accuracy: {train_acc:.4f}")
+    print(f"Test Accuracy: {test_acc:.4f}")
+
+    model.save_model("xgboost_dyslexia_model_v1.json")"""
+
+    print(X_train[0])
+    model = XGBoostModel.from_file("xgboost_dyslexia_model_v1.json")
+    # Example prediction (replace with actual data)
+    prediction = model.predict(X_train[0].reshape(1, -1))
+    prediction_proba = model.predict_proba(X_train[0].reshape(1, -1))
+    print(f"Predicted class: {prediction[0]}")
+    print(f"Predicted proba: {prediction_proba[0][prediction[0]]}")
+    print(f"True class: {y_train[0]}")
+
+if __name__ == "__main__":
+    main()
