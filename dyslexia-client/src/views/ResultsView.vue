0e0de776503ed3d98b0f0c6e4a4b0bd9561ea2bb
maelle rabouan <maelle.rabouan@gmail.com>
2026-06-08 15:53:15 +0200

    add base result

--- dyslexia-client/src/views/ResultsView.vue
+++ dyslexia-client/src/views/ResultsView.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
+  <div class="max-w-4xl mx-auto px-4 py-8 mt-15">
     <h1>Résultats</h1>
     <div v-if="!store.data">
       <p>Aucune session enregistrée.</p>
-      <RouterLink to="/test">Faire le test</RouterLink>
+      <RouterLink to="/test"
+        class="mt-8 inline-block rounded-full border px-6 py-3 text-black hover:bg-gray-50"
+      >Faire le test</RouterLink>
     </div>
     <div v-else>
       <div>
         <p> les Graphiques et resultats ici</p>
       </div>
-      <button @click="store.clear()">Effacer et recommencer</button>
+      <button @click="store.clear()"
+        class="mt-8 rounded-full border px-6 py-3 text-black hover:bg-gray-50"
+      >Effacer et recommencer</button>
     </div>
   </div>
 </template>
